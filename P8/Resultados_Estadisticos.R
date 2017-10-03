108c4ad314556ee8ed8e36040549d6fde8c26207
angelesmttz <angelesmttz@live.com.mx>
2017-10-02 19:36:03 -0500

    Avance
    Avance

---
+++ P8/Resultados_Estadisticos.R
@@ -0,0 +1,46 @@
+Welch Two Sample t-test
+
+data:  dat.org and dat.par
+t = -13.414, df = 7.6286, p-value = 1.409e-06
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  -1.482016 -1.044061
+sample estimates:
+  mean of x mean of y 
+0.6701166 1.9331548 
+
+
+Welch Two Sample t-test
+
+data:  dat.org and dat.par
+t = -2.8868, df = 6.3416, p-value = 0.02613
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  -1.6439349 -0.1461892
+sample estimates:
+  mean of x mean of y 
+2.974080  3.869142 
+
+
+Welch Two Sample t-test
+
+data:  dat.org and dat.par
+t = 3.9009, df = 7.52, p-value = 0.005121
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  0.4693162 1.8643426
+sample estimates:
+  mean of x mean of y 
+7.243725  6.076895 
+
+
+Welch Two Sample t-test
+
+data:  dat.org and dat.par
+t = 15.398, df = 5.0382, p-value = 1.977e-05
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  4.868839 6.814955
+sample estimates:
+  mean of x mean of y 
+13.764368  7.922471 
