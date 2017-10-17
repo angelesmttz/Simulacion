c502e256e0bec2727cd11fddd5efe07809c3fd3e
Angeles Martinez <30841288+angelesmttz@users.noreply.github.com>
2017-10-17 01:54:54 -0500

    Final
    Final

--- P10/Cod_P10_R1.R
+++ P10/Cod_P10_R1.R
@@ -1,5 +1,5 @@
-ciclos<-7
-tmax<-1
+init<-50
+tmax<-50
 
 library(testit)
 
@@ -92,8 +92,8 @@ para.mut<-function(i){
   }
 }
 
-para.rep<-function(i){
-  padres <- sample(1:tam, 2,replace=TRUE)
+para.rep.m<-function(i){
+  padres <- sample(1:tam, 2, prob=elites,replace=TRUE)
   hijos <- reproduccion(p[padres[1],], p[padres[2],], n)
   p.hijo<- hijos[1:n] # primer hijo
   s.hijo<- hijos[(n+1):(2*n)] # segundo hijo
@@ -108,6 +108,7 @@ para.obj<-function(i){
   return(datos)
 }
 
+
 n <- 50
 pesos <- generador.pesos(n, 15, 80)
 valores <- generador.valores(pesos, 10, 500)
@@ -116,35 +117,83 @@ optimo <- knapsack(capacidad, pesos, valores)
 pm <- 0.05
 rep <- 50
 resultados<-data.frame()
-for (replicas in 1:ciclos){
-  for (init in seq(200,500,100)){
-    
-    source('~/GitHub/Simulacion/Simulacion/P10/Cod_P10_T10.R')
-    t.paralel<-cbind("paralelo",replicas, init,t)
-    
-    source('~/GitHub/Simulacion/Simulacion/P10/Cod_P10.R')
-    t.original<-cbind("original",replicas,init,t)
-    
-    resultados<-rbind(resultados,t.paralel,t.original)
-    
-    
-  }
+
+
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+
+for (replicas in 1:5){
+p <- poblacion.inicial(n, init)
+tam <- dim(p)[1]
+assert(tam == init)
+mejores <- double()
+
+
+for (iter in 1:tmax) {
+  p$obj <- NULL
+  p$fact <- NULL
+  
+  p<-rbind(p,foreach(i=1:tam,.combine = rbind)%dopar% para.mut(i))
+  elites<-foreach(i=1:tam,.combine = c)%dopar% objetivo(p[i,], valores)
+  elites <- elites / sum(elites)
+  p<-rbind(p,foreach(i=1:rep,combine=rbind)%dopar% para.rep.m(i))
+  
+  tam <- dim(p)[1]
+  obj <- double()
+  fact <- integer()
+  
+  p<- data.frame(sapply(p, function(x) as.numeric(as.character(x))))
+  p<-cbind(p,foreach(i=1:tam,.combine=rbind)%dopar% para.obj(i))
+  
+  mantener <- order(-p[, (n + 2)], -p[, (n + 1)])[1:init]
+  
+  p <- p[mantener,]
+  tam <- dim(p)[1]
+  assert(tam == init)
+  factibles <- p[p$fact == TRUE,]
+  mejor <- max(factibles$obj)
+  mejores <- c(mejores, mejor)
+  
+}
+  datos<-cbind(replicas,mejores,c(1:tmax))
+  resultados<-rbind(resultados,datos)
   print(replicas)
 }
 
-save.image(file="Resultados_Tarea.RData")
+stopImplicitCluster()
+
+#save.image(file="Reto1.RData")
+
+para.rep<-function(i){
+  padres <- sample(1:tam, 2,replace=TRUE)
+  hijos <- reproduccion(p[padres[1],], p[padres[2],], n)
+  p.hijo<- hijos[1:n] # primer hijo
+  s.hijo<- hijos[(n+1):(2*n)] # segundo hijo
+  son<-rbind(p.hijo,s.hijo)
+  return(son)
+}
+
+source('~/GitHub/Simulacion/Simulacion/P10/Cod_P10_T10.R')
+tarea<-cbind(mejores,c(1:tmax))
+tarea<-as.data.frame(tarea)
+colnames(tarea)<-c("Valores","Paso")
+colnames(resultados)<-c("Replica","Valores","Paso")
+resultados$Replica<-as.factor(resultados$Replica)
+
 
 library(ggplot2)
+png("Seleccion_ruleta_100.png")
 
-colnames(resultados)<-c("Tipo","Replica","Init","Tiempo")
-resultados$Tiempo<-as.numeric(levels(resultados$Tiempo))[resultados$Tiempo]
-resultados$Tipo <- as.factor(resultados$Tipo)
-resultados$Init <- as.factor(resultados$Init)
+ggplot()+
+  geom_line(data=resultados,aes(x=resultados$Paso,y=resultados$Valores,color=resultados$Replica),size=0.6,linetype="F1")+
+  geom_hline(yintercept=optimo, linetype="dashed", color = "darkgreen",size=0.6)+
+  geom_line(data=tarea,aes(x=tarea$Paso,y=tarea$Valores,color="black"),size=0.6)+
+  xlab("Paso")+   ylab("Valores")+
+  scale_color_manual(name = "Selección", values = c("deepskyblue","deepskyblue1",
+          "darkgoldenrod2","firebrick3","darkorchid","black"),
+          labels =c("Ruleta1", "Ruleta2","Ruleta3","Ruleta4","Ruleta5","Original")) +
+    guides(color = guide_legend(order = 1))
+    dev.off()
 
-png("Variacion_pob.png")
-ggplot(data=resultados, aes(x = Init, y= Tiempo,fill = Tipo)) +
-  geom_boxplot(position=position_dodge(1))+
-  ylab("Tiempo (s)") +
-  xlab("Población")
-dev.off()
 
+save.image(file="R1_100.RData")
