cad1d029d8a47111437bfd222190593a83ccaefc
angelesmttz <angelesmttz@live.com.mx>
2017-12-07 15:26:16 -0600

    Avance
    Union de Plata
    * Desplazamiento Ag

--- Proyecto/Polimero_carboxilos.R
+++ Proyecto/Polimero_carboxilos.R
@@ -6,7 +6,7 @@ mw<-250*(10**3) #k(g/mol)
 DS=1.2
 temperatura<-90 #Centigrados
 tiempo<-24 #horas
-reduccion<-0.4*(10**-17)
+reduccion<-0.4*(10**-19)
 
 ## Calculos ###
 avogrado<-6.023*(10**23) #avogrado
@@ -18,13 +18,13 @@ atomos.plata<- round(avogrado*plata*reduccion/masa.plata)
 posiciones<-c(2,3,5)
 
 polimerizacion<-function (){
-  base<-rep("P",7)
+  base<-rep(NA,7)
   
   for (j in 1:length(posiciones)){
     cation<-posiciones[j]
     if (runif(1,0,1)<(DS/3)){
-      base[cation]<-"C"
-    }
+      base[cation]<-0
+    }#else{base[cation]<-"H"}
   }
   return(base)
 }
@@ -45,34 +45,107 @@ cadenas$y<-0
 cadenas[1,]$x<-runif(1,0,l)
 cadenas[1,]$y<-runif(1,0,l)
 
-paso<-l/100
-dimension<-c(1:2)
-dim(cadenas)[1]
+dimension<-2
 
-for (g in 2:dim(cadenas)[1]){
+colocar<-dim(cadenas)[1]
+
+for (g in 2:colocar){
   
-    for (d in 1:length(dimension)){
+    for (d in 1:dimension){
+    
+    paso<-l/100
       
-    if(runif(1)<0.5){
-       cadenas[g,d+1]<-cadenas[g-1,d+1]-paso}else{
-         cadenas[g,d+1]<-cadenas[g-1,d+1]+paso  
-       }
-      
-     # if (cadenas[g,d+1]<0){cadenas[g,d+1]<-cadenas[d,d+1]+l}
-      #if (cadenas[g,d+1]>l){cadenas[g,d+1]<-cadenas[d,d+1]-l}
+    if(runif(1)< 0.5){paso<--paso}
+    
+    cadenas[g,d+1]<-cadenas[g-1,d+1]+paso
       
     }
-    print(round(g*100/dim(cadenas)[1]))
+  
+
+    print(round(g*100/colocar))
 }
+
+
+# Considerar que el polimero no es lineal si no ciclico
+# Considerar la repulsión electroestática de los grupos funcionales 
+
+cadenas$x<-cadenas$x-min(cadenas$x)
+cadenas$y<-cadenas$y-min(cadenas$y)
 
 library(ggplot2)
 polymer<-ggplot()+
   geom_point(data=cadenas,aes(x=cadenas$x,y=cadenas$y,color=factor(cadenas$polimero)))+
-  xlab("x")+ylab("y")
+  xlab("x")+ylab("y")+
+  theme_bw()
+
+
+atomos.plata<-atomos.plata+40
 
 Ag<- data.frame(x = runif(atomos.plata,min(cadenas$x),max(cadenas$x)),
                 y=runif(atomos.plata,min(cadenas$y),max(cadenas$y)))  
 
 polymer+
-  geom_point(data=Ag,aes(x=Ag$x,y=Ag$y))
+  geom_point(data=Ag,aes(x=Ag$x,y=Ag$y),color="darkgoldenrod1")
+ggsave("Inicial.png")
 
+#save.image(file="P_C_Listo.RData")
+
+
+##############Velocidad####
+
+R<- 8.3144598 #(Kg)(m^2)/(s^2)(k)(mol)
+T.k<-temperatura+273
+mol.plata<- masa.plata*(10^-3)
+k<- 1.38 *(10**-23) #J/K
+Ag.v<- sqrt(3*R*T.k/mol.plata) #m/s
+KE<- (3/2)*k*T.k
+forma<-function(T.k){
+  Energy<-(3/2)*k*T.k
+  
+  return(2*Energy/mol.plata)
+}
+speed<-foreach(T.k=0:1000,.combine = c)%dopar% forma(T.k)
+curva<-data.frame(T.e=c(0:1000),Velocidad=speed)
+
+################Union####
+
+radio<-0.005
+p<-0.5
+Ag$estado<-"F"
+
+condicional<-matrix(FALSE,ncol=atomos.plata,nrow=colocar)
+
+  for (r in 1:colocar){
+    
+    if (is.na(cadenas[r,]$polimero)){
+      
+    cadenas[r,]$polimero<-cadenas[r,]$polimero
+      
+    }else{
+  
+    for (u in 1:atomos.plata){
+      if (Ag$estado=="F"){
+      
+      dx<-Ag[u,]$x-cadenas[r,]$x
+      dy<-Ag[u,]$x-cadenas[r,]$x
+      d<- sqrt(dx^2 + dy^2)
+      
+      if (d<radio){
+        if (runif(1)<p){
+        condicional[r,u]<-TRUE
+        cadenas[r,]$polimero<-cadenas[r,]$polimero+1
+        Ag[u,]$estado<-"O"
+        Ag[u,]$x<-cadenas[r,]$x
+        Ag[u,]$y<-cadenas[r,]$y
+        }
+        
+      }
+      
+      }
+    }
+    }
+  }
+
+polymer+
+  geom_point(data=Ag,aes(x=Ag$x,y=Ag$y),color="darkgoldenrod1")
+ggsave("Paso.png")
